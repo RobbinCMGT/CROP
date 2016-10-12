fdbacacb25c5f4f679ec0451ea4cacc4b094c18e
perrydrums <psperryjanssen@gmail.com>
2016-10-12 15:06:27 +0200

    Changed a whole fucking lot

--- CROPWEBSITE/files/header.php
+++ CROPWEBSITE/files/header.php
@@ -22,7 +22,7 @@
 
 	<head>
 		<title>CROP</title>
-		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="stylesheet" type="text/css" href="files/style.css"/>
 	</head>
 	
 	<body>
