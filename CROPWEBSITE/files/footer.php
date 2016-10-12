82feb134c91a584989cdc5805de2e3447227f371
Robbin Scheurwater <RobbinCMGT@Gmail.com>
2016-10-12 15:51:52 +0200

    CSS veranderd

--- CROPWEBSITE/files/footer.php
+++ CROPWEBSITE/files/footer.php
@@ -2,6 +2,4 @@
 
 <div id="footer">
 
-	<img src="images/Logo.png">
-
 </div>
--- CROPWEBSITE/index.php
+++ CROPWEBSITE/index.php
@@ -1,4 +1,8 @@
+
+
+
 <?php include("files/header.php"); ?>
+<!-- Vanaf hier begint pagina -->
 <div id="wrapper">
 	
 	
