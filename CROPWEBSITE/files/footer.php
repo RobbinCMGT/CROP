813808ac784621b029b47f3ae23cad493ebf1592
Robbin Scheurwater <RobbinCMGT@Gmail.com>
2016-10-12 14:59:52 +0200

    De vormen een beetje aangepast

--- CROPWEBSITE/files/footer.php
+++ CROPWEBSITE/files/footer.php
@@ -1,9 +1,7 @@
-<div id="footer" xmlns:height="http://www.w3.org/1999/xhtml">
-     <img src="files/images/Logo.png" alt="CROP" width="100vh"
 
-	</div>
 
-</body>
+<div id="footer">
 
-</html>
+	<img src="images/Logo.png">
 
+</div>
