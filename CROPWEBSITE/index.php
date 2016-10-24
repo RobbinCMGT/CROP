c15f93b4abacc05b6903d61f765b9f44eaa998d8
perrydrums <psperryjanssen@gmail.com>
2016-10-24 12:23:57 +0200

    Borders af

--- CROPWEBSITE/index.php
+++ CROPWEBSITE/index.php
@@ -23,9 +23,15 @@
 	</div>
 
 	<div id="content4">
-
+		<img src="files/images/about/Alicia.png" width="300">
+		<img src="files/images/about/Aniss.png" width="300">
+		<img src="files/images/about/Arno.png" width="300">
+		<img src="files/images/about/Chelsea.png" width="300">
+		<img src="files/images/about/Joel.png" width="300">
+		<img src="files/images/about/Perry.png" width="300">
+		<img src="files/images/about/Robbin.png" width="300">
+		<img src="files/images/about/Shane.png" width="300">
 	</div>
-
 
 	
 </div> <!-- Wrapper -->
