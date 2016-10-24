6261194fe515250a815dcb2af17fd0b38f815d8b
perrydrums <psperryjanssen@gmail.com>
2016-10-24 14:23:02 +0200

    style en index- about sectie

--- CROPWEBSITE/index.php
+++ CROPWEBSITE/index.php
@@ -1,6 +1,3 @@
-
-
-
 <?php include("files/header.php"); ?>
 <!-- Vanaf hier begint pagina -->
 <div id="wrapper">
@@ -15,7 +12,6 @@
 	<div id="content2">
         <br><br> <center> <h1> How it works </h1></center>
 
-
 	</div>
 
 	<div id="content3">
@@ -24,14 +20,47 @@
 
 	<div id="content4">
         <br><br> <center> <h1> About us </h1></center>
-		<img src="files/images/about/Alicia.png" width="300">
-		<img src="files/images/about/Aniss.png" width="300">
-		<img src="files/images/about/Arno.png" width="300">
-		<img src="files/images/about/Chelsea.png" width="300">
-		<img src="files/images/about/Joel.png" width="300">
-		<img src="files/images/about/Perry.png" width="300">
-		<img src="files/images/about/Robbin.png" width="300">
-		<img src="files/images/about/Shane.png" width="300">
+		<br><br>
+
+		<table>
+			<tr>
+				<td><img src="files/images/about/Alicia.png"></td>
+				<td><img src="files/images/about/Shane.png"></td>
+				<td><img src="files/images/about/Arno.png"></td>
+				<td><img src="files/images/about/Chelsea.png"></td>
+			</tr>
+			<tr>
+				<td>Alicia</td>
+				<td>Aniss</td>
+				<td>Arno</td>
+				<td>Chelsea</td>
+			</tr>
+			<tr>
+				<td class="aboutDes">Social Media</td>
+				<td class="aboutDes">Social Media</td>
+				<td class="aboutDes">Graphics</td>
+				<td class="aboutDes">Programming</td>
+			</tr>
+			<tr height="30px"><td>&nbsp;</td></tr>
+			<tr>
+				<td><img src="files/images/about/Joel.png"></td>
+				<td><img src="files/images/about/Perry.png"></td>
+				<td><img src="files/images/about/Robbin.png"></td>
+				<td><img src="files/images/about/Aniss.png"></td>
+			</tr>
+			<tr>
+				<td>Jo&euml;l</td>
+				<td>Perry</td>
+				<td>Robbin</td>
+				<td>Shane</td>
+			</tr>
+			<tr>
+				<td class="aboutDes">Programming</td>
+				<td class="aboutDes">Front-end Developer</td>
+				<td class="aboutDes">Front-end Developer</td>
+				<td class="aboutDes">Design</td>
+			</tr>
+		</table>
 	</div>
 
 
