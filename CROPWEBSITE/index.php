48dd390751404c34e788588c55ac519aff043311
Robbin Scheurwater <RobbinCMGT@Gmail.com>
2016-10-24 12:02:24 +0200

    Buttons veranderd enzo

--- CROPWEBSITE/index.php
+++ CROPWEBSITE/index.php
@@ -7,26 +7,26 @@
 	
 	
 	<div id="content1">
-		<br><br><br><br>
+		<br><br>  <center> <h1> The Product</h1> </center> <br><br>
 		<img src="files/images/mars food 01.png" width="300">
+
 	</div>
 
 	<div id="content2">
-        <br>
-        <br>
+        <br><br> <center> <h1> How it works </h1></center>
 
-    Hoi
+
 	</div>
 
 	<div id="content3">
-
+        <br><br> <center> <h1> Why </h1></center>
 	</div>
 
 	<div id="content4">
-
+        <br><br> <center> <h1> About us </h1></center>
 	</div>
 
 
-	
+
 </div> <!-- Wrapper -->
 <?php include("files/footer.php"); ?>
